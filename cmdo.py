8b6d571fe879fa2285759da6dd30e0536aadf7f2
kaundur <hello@kaundur.com>
2017-07-17 13:33:59 +0100

    Update

--- cmdo.py
+++ cmdo.py
@@ -16,12 +16,26 @@ class TodoList:
 
         self.parser.add_argument('-l', '--list', help="", action="store_true")
         self.parser.add_argument('-a', '--add', help="", type=str)
+        self.parser.add_argument('--due', help="", type=str)
         self.parser.add_argument('-r', '--remove', help="", type=int)
         self.parser.add_argument('-d', '--done', help="", type=int)
+        self.parser.add_argument('-t', '--debug', help="",  action="store_true")
         args = self.parser.parse_args()
-        self.handle_arguments(args)
+
+        if self.check_arguments(args):
+            self.handle_arguments(args)
+        else:
+            self.display.display_welcome()
 
         self.dal.close_connection()
+
+    def check_arguments(self, args):
+        any_arg_set = False
+        for arg in vars(args):
+            if getattr(args, arg):
+                any_arg_set = True
+                break
+        return any_arg_set
 
 
     def handle_arguments(self, args):
@@ -29,12 +43,19 @@ class TodoList:
         if args.list:
             self.display_list()
         if args.add:
-            self.dal.add_to_cmdo_list(args.add)
+            due_date = None
+            if args.due:
+                due_date = args.due
+
+            self.dal.add_to_cmdo_list(args.add, due_date)
         if args.remove:
             self.dal.remove_by_id(args.remove)
         if args.done:
             self.dal.mark_as_done(args.done)
             self.display_list()
+        if args.debug:
+            self.dal.dump_database()
+
 
     def display_list(self):
         cmdo_list = self.dal.get_cmdo_list()
--- dal.py
+++ dal.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 
 
 class DAL:
@@ -6,7 +7,6 @@ class DAL:
         self.database_name = 'cmdo.db'
         self.database_connection = sqlite3.connect(self.database_name)
         self.initalise_database()
-
 
     def initalise_database(self):
         self.create_main_table()
@@ -18,19 +18,25 @@ class DAL:
                       (date TIMESTAMP DEFAULT (datetime('now','localtime')),
                       title text,
                       priority int,
+                      due DATE,
                       complete BOOLEAN NOT NULL CHECK (complete IN (0, 1)) DEFAULT (0)
                       )''')
         self.database_connection.commit()
 
     def get_cmdo_list(self):
         cursor = self.database_connection.cursor()
-        cmdo_list = cursor.execute('SELECT rowid, date, title, priority, complete FROM todo_list')
+        # TODO - This doesnt link to the interface well, have to define everything here as well as in display
+        cmdo_list = cursor.execute('SELECT rowid, date, title, priority, complete, due FROM todo_list')
         return cmdo_list
 
-    def add_to_cmdo_list(self, message, priority=0):
+    def add_to_cmdo_list(self, message, due_date, priority=0):
         cursor = self.database_connection.cursor()
-        print 'Added "'+message+'" to todo list with priority',priority
-        cursor.execute("INSERT INTO todo_list (title, priority) VALUES (?,0 )", (message,))
+        print 'Added "'+message+'" to todo list with priority', priority
+        if due_date:
+            # due_date = datetime.datetime.today()
+            cursor.execute("INSERT INTO todo_list (title, priority, due) VALUES (?, ?, ? )", (message, priority, due_date, ))
+        else:
+            cursor.execute("INSERT INTO todo_list (title, priority) VALUES (?, ? )", (message, priority,))
         self.database_connection.commit()
 
     def remove_by_id(self, item_id):
